24a78c1281c2ae7d3f551d53225da6fe6cafb910
Brandyn Britton <brandynbb96@gmail.com>
2025-12-24 01:23:29 +0800

    feat(auth): removed SSL from auth reqs

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,6 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const auth = betterAuth({
   database: new Pool({
     connectionString: process.env.POSTGRES_URL!,
+    ssl: false
   }),
   emailAndPassword: {
     enabled: true,
